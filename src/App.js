e880e01f281dc565483247e159044117942e5c64
manasvislj <manasvisaluja2003@gmail.com>
2023-11-17 01:48:55 -0500

    Edited app.js to run tests

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import {Routes, Route} from "react-router-dom"
+import Home from "./screens/Home.jsx"
+import Character from './components/Characters.jsx';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Character />
     </div>
   );
 }
