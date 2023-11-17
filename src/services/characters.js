7bd7b7741ad4c7f0f81c8865a46df86529375e7c
manasvislj <manasvisaluja2003@gmail.com>
2023-11-17 10:33:37 -0500

    Added characters. js file in services folder

---
+++ src/services/characters.js
@@ -0,0 +1,42 @@
+import api from "./apiConfig"
+// We are doing postman stuff but coding it.
+export const getCharacters = async () => {
+    try {
+        const characters = await api.get('/characters')
+        return characters.data
+    } catch (error) {
+        console.log('Error:getting all characters: ', error)
+    }
+}
+export const getCharacter = async (id) => {
+    try {
+        const response = await api.get(`/characters/${id}`)
+        return response.data
+    } catch (error) {
+        console.log('Error: getting one character: ', error)
+    }
+}
+export const createCharacter = async (characterData) => {
+    try {
+        const response = await api.post('/characters', characterData)
+        return response.data
+    } catch (error) {
+        console.log('Error: creating one character: ', error)
+    }
+}
+export const editCharacter = async (id , characterData) => {
+    try {
+        const response = await api.put(`/character/${id}`, characterData)
+        return response.data
+    } catch (error) {
+        console.log('Error:editing one character: ', error)
+    }
+}
+export const deleteCharacter = async (id) => {
+    try {
+      const response = await api.delete(`/characters/${id}`);
+      return response.data;
+    } catch (error) {
+      console.error(error);
+    }
+  };
