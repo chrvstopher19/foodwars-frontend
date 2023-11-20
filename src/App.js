d63a3fedcd0231edaaf98d9e6891ac65bb301f13
manasvislj <manasvisaluja2003@gmail.com>
2023-11-20 01:35:12 -0500

    Finished on front-end authentication

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import './App.css';
-import {Routes, Route} from "react-router-dom"
+import {Routes, Route, Navigate} from "react-router-dom"
+import { useState, useEffect } from "react";
 import Home from "./screens/Home.jsx"
 import Characters from './screens/Characters.jsx';
 import Detail from './screens/CharacterDetail.jsx';
 import Dishes from "./screens/Dishes.jsx"
 import DishDetails from "./screens/DishesDetail.jsx"
 import DishCreate from './screens/DishesCreate.jsx';
+import { verifyUser } from "./services/users.js";
+import SignUp from "./screens/SignUp.jsx";
+import SignIn from "./screens/SignIn.jsx";
+import SignOut from "./screens/SignOut.jsx";
 
-function App() {
+const App = () => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const user = await verifyUser();
+      user ? setUser(user) : setUser(null);
+    };
+    fetchUser();
+  }, []);
+
   return (
     <div className="App">
       <Routes>
@@ -17,6 +32,9 @@ function App() {
         <Route path="/dishes" element={<Dishes />}/>
         <Route path="/dishes/:dishesId" element={<DishDetails />} />
         <Route path="/add-dish" element={<DishCreate />} />
+        <Route path="/sign-up" element={<SignUp setUser={setUser} />} />
+        <Route path="/sign-in" element={<SignIn setUser={setUser} />} />
+        <Route path="/sign-out" element={<SignOut setUser={setUser} />} />
       </Routes>
     </div>
   );
