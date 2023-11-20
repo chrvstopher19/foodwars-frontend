8768bcc03262f68cfd20d92e6f637159cd0e334b
manasvislj <manasvisaluja2003@gmail.com>
2023-11-20 18:56:50 -0500

    CSS for recipes

--- src/components/Dish.jsx
+++ src/components/Dish.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import "../css/Dishes.css"
 
 function Dish({dish}) {
   return (
     <Link to={`/dishes/${dish._id}`}>
-        <div>
-            <h1>{dish.dish_name}</h1>
-            <img src={dish.img_link} alt={dish.dish_name}/>
+        <div className="dish-img-container">
+            <h1 className="dish-name">{dish.dish_name}</h1>
+            <img className="dish-img" src={dish.img_link} alt={dish.dish_name}/>
         </div>
     </Link>
   )
